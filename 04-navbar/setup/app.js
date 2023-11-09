58cd835a16a75d7498ad34583d2c4924839523df
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-09 00:00:47 -0800

    finish project 4: nav bar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,14 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+toggleBtn = document.querySelector('.nav-toggle');
+links = document.querySelector('.links');
+toggleBtn.addEventListener('click', () => {
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links');
+    // }
+    // else {
+    //     links.classList.add('show-links');
+    // }
+    links.classList.toggle('show-links');
+});
