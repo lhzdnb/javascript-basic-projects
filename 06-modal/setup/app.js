b6cb465f94b9bc2413736e6c5be608a34fc5c5da
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-10 15:43:28 -0800

    finish project 6: modal

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,14 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const modelBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modelOverlay = document.querySelector('.modal-overlay');
+
+modelBtn.addEventListener('click', () => {
+    modelOverlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+    modelOverlay.classList.remove('open-modal');
+});
