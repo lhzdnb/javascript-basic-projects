131c0b8585b75eebefd5c772ca31898cfd308dad
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-10 16:52:50 -0800

    finish project 7: questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+questions.forEach(question => {
+    const button = question.querySelector('.question-btn');
+    button.addEventListener('click', event => {
+        questions.forEach(q => {
+            if (q !== question) {
+                q.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
+    });
+});
+
+
 // traversing the dom
+
+// const questionButtons = document.querySelectorAll('.question-btn');
+// questionButtons.forEach(plusIcon => {
+//     plusIcon.addEventListener('click', (event) => {
+//         const question = event.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
