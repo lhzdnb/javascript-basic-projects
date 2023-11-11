b8457e2887fa71e6dce35d9dfa33e94dc6e35556
lh20020727 <lh20020727@sjtu.edu.cn>
2023-11-11 14:32:26 -0800

    Finish Project 12: countdown-timer

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,53 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 10, 13, 11, 30);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getHours();
+const month = futureDate.getMonth();
+const date = futureDate.getDate();
+const day = futureDate.getDay();
+const am = hours >= 12 ? "PM" : "AM";
+
+giveaway.textContent = `giveaway ends on ${weekdays[day]}, ${date} ${months[month]} ${year} ${hours}:${mins}${am}`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainigTime() {
+  const today = new Date().getTime();
+  let temp = futureTime - today;
+  const remainingDays = Math.floor(temp / (24 * 60 * 60 * 1000));
+  temp %= 24 * 60 * 60 * 1000;
+  const remainingHours = Math.floor(temp / (60 * 60 * 1000));
+  temp %= 60 * 60 * 1000;
+  const remainingMins = Math.floor(temp / (60 * 1000));
+  temp %= 60 * 1000;
+  const remainingSecs = Math.floor(temp / 1000);
+  temp %= 1000;
+  return [remainingDays, remainingHours, remainingMins, remainingSecs];
+}
+
+function format(item) {
+  if (item < 10) {
+    return item.toString().padStart(2, "0");
+  } else {
+    return item;
+  }
+}
+
+// count down
+setInterval(() => {
+  const remainingTime = getRemainigTime();
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(remainingTime[index]);
+  });
+}, 1000);
